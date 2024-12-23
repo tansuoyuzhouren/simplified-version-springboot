29291082feaa3523e49fdfcb481badfa10a58aa9
root <3358817353@qq.com>
2024-12-23 22:56:23 +0800

    first commit simplified version springboot

---
+++ springboot/src/main/java/com/simplified/springboot/Core/SpringApplication.java
@@ -0,0 +1,8 @@
+package com.simplified.springboot.Core;
+
+public class SpringApplication {
+
+    public static void run(Class zclass){
+        System.out.println("启动springBoot");
+    }
+}
---
+++ user/src/main/java/com/kuang/Application.java
@@ -0,0 +1,9 @@
+package com.kuang;
+
+import com.simplified.springboot.Core.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
